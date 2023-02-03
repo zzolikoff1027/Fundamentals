dc09f7a15ba699eac5c15f9100e012b6f790663c
zzolikoff1027 <78103329+zzolikoff1027@users.noreply.github.com>
2023-02-03 12:08:11 -0700

    Fixing Wander

--- Fundamentals/Assets/Scripts/Behaviors/Seek.cs
+++ Fundamentals/Assets/Scripts/Behaviors/Seek.cs
@@ -45,3 +45,23 @@ public class Seek : SteeringBehavior
         return result;
     }
 }
+
+public class Wander : Seek
+{
+    float delay = 1.0f;
+    float elapsedTime = 4.0f;
+    Vector3 result;
+
+    protected override Vector3 getTargetPosition()
+    {
+        elapsedTime = Time.deltaTime;
+        if(elapsedTime > delay)
+        {
+            int offset = 100;
+            Vector3 circlePos = Random.insideUnitCircle * offset;
+            Vector3 adjustedCirclePos = new Vector3(circlePos.x, 0, circlePos.y);
+            result = character.transform.position + (character.transform.forward * 10) + adjustedCirclePos;
+        }
+        return result;
+    }
+}
--- Fundamentals/Assets/Scripts/Wanderer.cs
+++ Fundamentals/Assets/Scripts/Wanderer.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class Wanderer : Kinematic
 {
-    public Wander myRotateType;
+    Wander myMoveType;
+    Face mySeekRotateType;
+    LookWhereGoing myFleeRotateType;
 
+    public bool flee = false;
+
+    // Start is called before the first frame update
     void Start()
     {
-        myRotateType = new Wander();
-        myRotateType.character = this;
-        myRotateType.target = myTarget;
+        myMoveType = new Wander();
+        myMoveType.character = this;
+        myMoveType.target = myTarget;
+        myMoveType.flee = flee;
+
+        mySeekRotateType = new Face();
+        mySeekRotateType.character = this;
+        mySeekRotateType.target = myTarget;
+
+        myFleeRotateType = new LookWhereGoing();
+        myFleeRotateType.character = this;
+        myFleeRotateType.target = myTarget;
     }
 
     // Update is called once per frame
     protected override void Update()
     {
         steeringUpdate = new SteeringOutput();
-        steeringUpdate.angular = myRotateType.getSteering().angular;
+        steeringUpdate.linear = myMoveType.getSteering().linear;
+        steeringUpdate.angular = flee ? myFleeRotateType.getSteering().angular : mySeekRotateType.getSteering().angular;
         base.Update();
     }
 }
